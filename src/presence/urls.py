d4cca62407b0c593dd3369af532d5f2db385f817
Shishir <sishishir2015@gmail.com>
2019-10-02 22:03:23 +0600

    6 Pages Created and Nemed Route Are Done

--- src/presence/urls.py
+++ src/presence/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import index
+from .views import (
+    index,
+    home,
+    book_list,
+    member_list,
+    add_book,
+    about,
+
+)
 
 
 urlpatterns = [
 
     path("",index,name='index'),
+    path("Home.py",home,name="home"),
+    path("Book-List.py",book_list,name="book-list"),
+    path("Member-List.py",member_list,name="member-list"),
+    path("Add-Book.py",add_book,name="Add-Book"),
+    path("About-Us.py",about,name="About")
 ]
--- src/presence/views.py
+++ src/presence/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 
+#1
 def index(req):
     context = {
 
     }
     return render(req,"presence/index.html",context=context)
+#2
+def home(req):
+
+    context={
+
+    }
+    return render(req,"presence/home.html",context=context)
+#3
+def book_list(req):
+
+    context={
+
+    }
+    return render(req,"presence/book_list.html",context=context)
+#4
+def member_list(req):
+
+    context={
+
+    }
+    return render(req,"presence/member_list.html",context=context)
+
+#5
+def add_book(req):
+    context={
+
+    }
+    return render(req,"presence/add_book.html",context=context)
+
+#6
+def about(req):
+    context={
+
+    }
+    return render(req,"presence/about.html",context=context)
+
+
